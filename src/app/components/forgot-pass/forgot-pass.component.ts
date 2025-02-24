c089c9ca0c4c0905a68b06c0c360ad9ab20e8548
maddusagari subahan <maddusagarisubahan@gmail.com>
2025-02-24 14:05:07 +0530

    dummy

--- src/app/components/forgot-pass/forgot-pass.component.ts
+++ src/app/components/forgot-pass/forgot-pass.component.ts
@@ -33,7 +33,7 @@ export class ForgotPassComponent {
     const email = this.forgotPasswordForm.value.email;
 
     this.http
-      .post('http://localhost:8000/api/todos/forgotpassword', { email })
+      .post('https://todoapp-backend-i8km.onrender.com/api/todos/forgotpassword', { email })
       .subscribe({
         next: (response: any) => {
           this.isSubmitting = false;
